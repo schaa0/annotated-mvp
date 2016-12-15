032393efaacb004c9de026bc1add799a375ec393
Andreas Schattney <a.schattney@gmail.com>
2016-12-15 01:59:07 +0100

    update

--- sample/src/main/java/com/mvp/example/photostream/view/viewcontract/IMainActivityView.java
+++
@@ -1,9 +0,0 @@
-package com.mvp.example.photostream.view.viewcontract;
-
-import com.mvp.MvpView;
-
-import java.io.IOException;
-
-public interface IMainActivityView extends MvpView{
-    void showError(IOException e);
-}
